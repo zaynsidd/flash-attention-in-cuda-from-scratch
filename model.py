7c66e6e3c35e9ff757b3561db0eb2483dbecd9dc
Zayn Siddique <89875116+zaynsidd@users.noreply.github.com>
2026-07-05 00:28:38 -0400

    Step 2: scale_array

--- model.py
+++ model.py
@@ -16,8 +16,15 @@ __global__ void vector_add(const float* a, const float* b, float* c, int n) {
     }
 }
 
-# Step 2 - scale_array (not yet solved)
-# TODO: implement
+# Step 2 - scale_array
+__global__ void scale_array(float* a, float scalar, int n) {
+    // TODO: multiply each element of a by scalar in place
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+
+    if(i<n){
+        a[i] *= scalar;
+    }
+}
 
 # Step 3 - elementwise_exp (not yet solved)
 # TODO: implement
